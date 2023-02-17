0b7ff7ce923141f7c56d8d42fb42678ca4e0d331
Kevin Gosse <kevin.gosse@datadoghq.com>
2023-02-17 15:18:59 +0100

    Use COMPlus for compatibility with older runtimes

--- profiler/test/Datadog.Profiler.IntegrationTests/LinuxOnly/WrapperLibraryTest.cs
+++ profiler/test/Datadog.Profiler.IntegrationTests/LinuxOnly/WrapperLibraryTest.cs
@@ -87,7 +87,7 @@ namespace Datadog.Profiler.IntegrationTests.LinuxOnly
             var runner = new TestApplicationRunner(appName, framework, appAssembly, _output, enableTracer: true, commandLine: "--scenario 7");
 
             runner.Environment.SetVariable("DD_TRACE_CRASH_HANDLER", "/usr/bin/echo");
-            runner.Environment.SetVariable("DOTNET_DbgEnableMiniDump", "1");
+            runner.Environment.SetVariable("COMPlus_DbgEnableMiniDump", "1");
 
             using var processHelper = runner.LaunchProcess();
 
@@ -103,7 +103,7 @@ namespace Datadog.Profiler.IntegrationTests.LinuxOnly
             var runner = new TestApplicationRunner(appName, framework, appAssembly, _output, enableTracer: true, commandLine: "--scenario 7");
 
             // Don't set DD_TRACE_CRASH_HANDLER. In that case, the call to createdump shouldn't be redirected
-            runner.Environment.SetVariable("DOTNET_DbgEnableMiniDump", "1");
+            runner.Environment.SetVariable("COMPlus_DbgEnableMiniDump", "1");
 
             using var processHelper = runner.LaunchProcess();
 
