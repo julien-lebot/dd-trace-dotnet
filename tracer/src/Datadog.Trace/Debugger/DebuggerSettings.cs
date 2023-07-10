4987ad1c36e79d7e295b0a23bb3a2b2854cc0f4a
dudik <dudi.keleti@gmail.com>
2023-07-10 10:11:05 +0200

    Initial commit for upload symbols

--- tracer/src/Datadog.Trace/Debugger/DebuggerSettings.cs
+++ tracer/src/Datadog.Trace/Debugger/DebuggerSettings.cs
@@ -22,6 +22,7 @@ namespace Datadog.Trace.Debugger
         public const int DefaultMaxNumberOfFieldsToCopy = 20;
 
         private const int DefaultUploadBatchSize = 100;
+        public const int DefaultMaxSymbolSizeToUpload = 1024 * 1024;
         private const int DefaultDiagnosticsIntervalSeconds = 60 * 60; // 1 hour
         private const int DefaultUploadFlushIntervalMilliseconds = 0;
 
@@ -49,6 +50,11 @@ namespace Datadog.Trace.Debugger
                              .AsInt32(DefaultUploadBatchSize, batchSize => batchSize > 0)
                              .Value;
 
+            MaxSymbolSizeToUpload = config
+                                         .WithKeys(ConfigurationKeys.Debugger.MaxSymbolSizeToUpload)
+                                         .AsInt32(DefaultMaxSymbolSizeToUpload, maxDepth => maxDepth > 0)
+                                         .Value;
+
             DiagnosticsIntervalSeconds = config
                                         .WithKeys(ConfigurationKeys.Debugger.DiagnosticsInterval)
                                         .AsInt32(DefaultDiagnosticsIntervalSeconds, interval => interval > 0)
@@ -75,6 +81,8 @@ namespace Datadog.Trace.Debugger
         public int MaximumDepthOfMembersToCopy { get; }
 
         public int UploadBatchSize { get; }
+
+        public int MaxSymbolSizeToUpload { get; }
 
         public int DiagnosticsIntervalSeconds { get; }
 
