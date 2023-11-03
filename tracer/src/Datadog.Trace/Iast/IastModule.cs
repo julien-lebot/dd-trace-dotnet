efa1e89ae8a9f286b266f6ad67fb26329f5c2f88
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2023-11-03 13:58:54 +0100

    Evidence nullable

--- tracer/src/Datadog.Trace/Iast/IastModule.cs
+++ tracer/src/Datadog.Trace/Iast/IastModule.cs
@@ -242,7 +242,7 @@ internal static class IastModule
     }
 
     // This method adds web vulnerabilities, with no location, only on web environments
-    private static Scope? AddWebVulnerability(string evidenceValue, IntegrationId integrationId, string vulnerabilityType, int hashId)
+    private static Scope? AddWebVulnerability(string? evidenceValue, IntegrationId integrationId, string vulnerabilityType, int hashId)
     {
         var tracer = Tracer.Instance;
         if (!iastSettings.Enabled || !tracer.Settings.IsIntegrationEnabled(integrationId))
@@ -264,7 +264,7 @@ internal static class IastModule
         var vulnerability = new Vulnerability(
             vulnerabilityType,
             hashId,
-            new Evidence(evidenceValue, null),
+            string.IsNullOrEmpty(evidenceValue) ? null : new Evidence(evidenceValue!, null),
             integrationId);
 
         if (!iastSettings.DeduplicationEnabled || HashBasedDeduplication.Instance.Add(vulnerability))
@@ -426,13 +426,7 @@ internal static class IastModule
     // hash('XCONTENTTYPE_HEADER_MISSING:<service-name>')
     internal static Scope? OnXContentTypeOptionsHeaderMissing(IntegrationId integrationId, string headerValue, string serviceName)
     {
-        var evidence = string.Empty;
-
-        if (!string.IsNullOrEmpty(headerValue))
-        {
-            evidence = headerValue;
-        }
-
+        string? evidence = string.IsNullOrEmpty(headerValue) ? null : headerValue;
         return AddWebVulnerability(evidence, integrationId, VulnerabilityTypeName.XContentTypeHeaderMissing, (VulnerabilityTypeName.XContentTypeHeaderMissing.ToString() + ":" + serviceName).GetStaticHashCode());
     }
 }
